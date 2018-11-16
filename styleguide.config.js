8dc72cde4c38f577085d4bb1ae106435b9358e5f
Gitlab-CI <dev@kopaxgroup.com>
2018-11-16 07:15:46 +0700

    fix(doc): added babel-polyfill to allow regenerator saga

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,7 @@
+const path = require('path');
 const { config } = require('@yeutech-lab/rollup-umd-documentation/lib/styleguide.config.js');
 
-module.exports = config;
+module.exports = {
+  ...config,
+  require: [path.resolve(__dirname, 'styleguide/setup.js')],
+};
